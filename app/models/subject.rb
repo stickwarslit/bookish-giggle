7c35d2790520d403e4b3833a4f66b64fd7c1f074
Mark Emmanuel Del Rosario <MarkESDR@gmail.com>
2019-07-04 17:39:23 -0400

    Lynda Chapter 5. Models and ActiveRecord

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+
+  scope :visible, -> { where(:visible => true) }
+  scope :invisible, -> { where(:visible => false) }
+
+  scope :sorted, -> { order(:position => :asc) }
+  scope :newest_first, -> { order(:created_at => :desc) }
+  scope :search, ->(query) { where(["name LIKE ?", "%#{query}%"]) }
 end
