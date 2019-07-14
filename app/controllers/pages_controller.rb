efdbfd0be037a4c52a298ab3bc240c48fb484f02
Mark Emmanuel Del Rosario <MarkESDR@gmail.com>
2019-07-14 16:23:21 -0400

    Add before_action funcs to page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   layout 'admin'
 
+  before_action :find_subjects, only: [:new, :create, :edit, :update]
+  before_action :set_page_count, only: [:new, :create, :edit, :update]
+
   def index
     @pages = Page.sorted
   end
@@ -12,8 +15,6 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted
   end
 
   def create
@@ -24,15 +25,12 @@ class PagesController < ApplicationController
       redirect_to(pages_path)
     else
       @page_count = Page.count + 1
-      @subjects = Subject.sorted
       render('new')
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count
-    @subjects = Subject.sorted
   end
 
   def update
@@ -43,8 +41,6 @@ class PagesController < ApplicationController
       flash[:notice] = "You have successfully updated your page"
       redirect_to(page_path)
     else
-      @page_count = Page.count + 1
-      @subjects = Subject.sorted
       render('edit')
     end
   end
@@ -64,5 +60,16 @@ class PagesController < ApplicationController
 
   def page_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_page_count
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @page_count += 1
+    end
   end
 end
